a2cdccb4181b88e736f7a5c04ea9efeba961c609
akshshu2 <akashtnas@gmail.com>
2018-01-18 23:46:07 +0530

    major change

--- game.js
+++ game.js
@@ -11,8 +11,12 @@ function distance(x1, x2, y1, y2) {
 }
 var mouse = {
     x: canvas.width / 2,
-    y: canvas.height - (20)
+    y: canvas.height - 10
 };
+window.addEventListener('mousemove', function(event) {
+    mouse.x = event.x;
+    mouse.y = canvas.width - 50
+})
 
 function rotate(velocity, angle) {
     const rotatedVelocities = {
@@ -20,6 +24,25 @@ function rotate(velocity, angle) {
         y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
     };
     return rotatedVelocities;
+}
+
+function intersects(circle, rect) {
+    var circleDistance_x = Math.abs(circle.x - (rect.x + rect.l / 2));
+    var circleDistance_y = Math.abs(circle.y - (rect.y + rect.b / 2));
+    if (circleDistance_x > (rect.l / 2 + circle.radius)) {
+        return false;
+    }
+    if (circleDistance_y > (rect.b / 2 + circle.radius)) {
+        return false;
+    }
+    if (circleDistance_x <= (rect.l / 2)) {
+        return true;
+    }
+    if (circleDistance_y <= (rect.b / 2)) {
+        return true;
+    }
+    var cornerDistance_sq = (circleDistance_x - rect.l / 2) ^ 2 + (circleDistance_y - rect.b / 2) ^ 2;
+    return (cornerDistance_sq <= (circle.radius ^ 2));
 }
 /**
  * Swaps out two colliding particles' x and y velocities after running through
@@ -54,6 +77,15 @@ function resolveCollision(particle, otherParticle) {
             y: u2.y
         };
         // Final velocity after rotating axis back to original location
+        const vFinal1 = rotate(v1, -angle);
+        const vFinal2 = rotate(v2, -angle);
+        // Swap particle velocities for realistic bounce effect
+        particle.velocity.x = vFinal1.x;
+        particle.velocity.y = vFinal1.y;
+        console.log(particle.velocity.x);
+        console.log(particle.velocity.y);
+        otherParticle.velocity.x = 0;
+        otherParticle.velocity.y = 0;
     }
 }
 
@@ -70,8 +102,8 @@ function Circle(x, y, radius, color) {
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = this.color;
-        c.stroke();
+        c.fillStyle = this.color;
+        c.fill();
         c.closePath();
     }
 }
@@ -81,30 +113,37 @@ function Ball(x, y, radius, color) {
     this.y = y;
     this.radius = radius;
     this.color = color;
-    this.mass = 1;
+    this.mass = 0;
     this.velocity = {
-        x: Math.random() * 3,
-        y: Math.random() * 3
+        x: 5,
+        y: 5
     };
     this.draw = function() {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        c.strokeStyle = this.color;
-        c.stroke();
+        c.fillStyle = this.color;
+        c.fill();
     }
-    this.update = function(circle_array) {
+    this.update = function(circle_array, bar) {
         for (var i = 0; i < circle_array.length; i++) {
             if (distance(this.x, circle_array[i].x, this.y, circle_array[i].y) - (this.radius + circle_array[i].radius) < 0) {
                 resolveCollision(this, circle_array[i]);
-                circle_array[i].radius -= 5;
-                this.dx = -this.dx;
-                this.dy = -this.dy;
+                circle_array[i].radius -= 20;
             }
+            if (circle_array[i].radius <= 0) {
+                circle_array.splice(i, 1);
+            }
+        }
+        if (intersects(this, bar) == true) {
+            this.velocity.y = -this.velocity.y;
+        }
+        if (this.y + this.radius > innerHeight) {
+            alert("gameover");
         }
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
             this.velocity.x = -this.velocity.x;
         }
-        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+        if (this.y - this.radius < 0) {
             this.velocity.y = -this.velocity.y;
         }
         this.x += this.velocity.x;
@@ -136,22 +175,44 @@ function init() {
     }
 }
 var strike_ball;
+var bar;
+
+function Bar(x, y, l, b) {
+    this.x = x;
+    this.y = y;
+    this.l = l;
+    this.b = b;
+    this.mass = 1;
+    this.velocity = {
+        x: 0,
+        y: 0
+    };
+    this.draw = function() {
+        c.fillStyle = "rgba(255,0,0,0.5)";
+        c.fillRect(this.x, this.y, this.l, this.b);
+    }
+    this.update = function() {
+        this.x = mouse.x;
+        this.draw();
+    }
+}
 
 function init2() {
     var radius = 8;
     var dx = ((Math.random() - 0.5) * 5);
     var dy = ((Math.random() - 0.5) * 5);
-    strike_ball = (new Ball(mouse.x, mouse.y, radius, "red"));
+    strike_ball = (new Ball(canvas.width / 2, canvas.height - 80, radius, "red"));
+    bar = new Bar(mouse.x, mouse.y, 100, 10);
 }
 
 function animate() {
     window.requestAnimationFrame(animate);
-    c.fillStyle = "#1e1e1e";
-    c.fillRect(0, 0, canvas.width, canvas.height);
-    strike_ball.update(circle_array);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    strike_ball.update(circle_array, bar);
     for (var k = 0; k < circle_array.length; k++) {
         circle_array[k].draw();
     }
+    bar.update();
 }
 init();
 init2();
